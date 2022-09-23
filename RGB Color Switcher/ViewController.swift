4cfd67bdc81098b0c4a23cae071660b448d1deab
enjoy2k <Enjoy2k@mail.ru>
2022-09-23 21:44:35 +0300

    Переписал не оптимизированное решение преподавателя. Дальше, в новой ветке, буду оптимизировать код

--- RGB Color Switcher/ViewController.swift
+++ RGB Color Switcher/ViewController.swift
@@ -24,33 +24,28 @@ class ViewController: UIViewController {
         
         colorPreview.layer.cornerRadius = 15
         
-        colorPreview.backgroundColor = UIColor(displayP3Red:
-        CGFloat(redSlider.value), green:
-        CGFloat(greenSlider.value), blue:
-        CGFloat(blueSlider.value), alpha: 1)
-
+        colorPreview.backgroundColor = UIColor( // В скобках я пишу инициализатор. Перед скобками же, я указываю тип данных. В данном случае я инициализирую цвет - UIColor.
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )   // Здесь прописываю этот код, чтобы он изначально загружал вью с заданным, нейтральным цветом из слайдеров
+        
         redIndicator.text = String(format: "%.2f", redSlider.value)
         greenIndicator.text = String(format: "%.2f", greenSlider.value)
         blueIndicator.text = String(format: "%.2f", blueSlider.value)
-    }
-    func colorChange() {
-        colorPreview.backgroundColor = UIColor(displayP3Red:
-        CGFloat(redSlider.value), green:
-        CGFloat(greenSlider.value), blue:
-        CGFloat(blueSlider.value), alpha: 1)
-    }
-    @IBAction func redSliderAction() {
+    }   // Здесь я передаю в лэйблы соответствующие значения слайдеров. Всё. Теперь при загрузке вью, все необходимые элементы интерфейса будут отображены корректно
+    
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        colorPreview.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        
         redIndicator.text = String(format: "%.2f", redSlider.value)
-        colorChange()
-    }
-    
-    @IBAction func greenSliderAction() {
         greenIndicator.text = String(format: "%.2f", greenSlider.value)
-        colorChange()
-    }
-    
-    @IBAction func blueSliderAction() {
         blueIndicator.text = String(format: "%.2f", blueSlider.value)
-        colorChange()
-    }
+    } // Вот здесь очень интересно получается) Каждый раз прикасаясь к любому из слайдеров, в соответствующий лэйбл передаётся значение. А два остальных слайдера значений не меняют, НО ОБНОВЛЯЮТ ИХ! Так как код воспроизводится каждый раз при касании слайдера! И это говорит мне о том что код не оптимальный, и его нужно оптимизировать
 }
