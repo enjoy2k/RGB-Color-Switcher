d893ba99d1cd4ed5d12a2057fed106e582b6e6b3
enjoy2k <Enjoy2k@mail.ru>
2022-09-23 22:02:00 +0300

    Правлю

--- RGB Color Switcher/ViewController.swift
+++ RGB Color Switcher/ViewController.swift
@@ -24,17 +24,12 @@ class ViewController: UIViewController {
         
         colorPreview.layer.cornerRadius = 15
         
-        colorPreview.backgroundColor = UIColor( // В скобках я пишу инициализатор. Перед скобками же, я указываю тип данных. В данном случае я инициализирую цвет - UIColor.
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )   // Здесь прописываю этот код, чтобы он изначально загружал вью с заданным, нейтральным цветом из слайдеров
+        setColor()
         
-        redIndicator.text = String(format: "%.2f", redSlider.value)
-        greenIndicator.text = String(format: "%.2f", greenSlider.value)
-        blueIndicator.text = String(format: "%.2f", blueSlider.value)
-    }   // Здесь я передаю в лэйблы соответствующие значения слайдеров. Всё. Теперь при загрузке вью, все необходимые элементы интерфейса будут отображены корректно
+        redIndicator.text = string(from: redSlider)
+        greenIndicator.text = string(from: greenSlider)
+        blueIndicator.text = string(from: blueSlider)
+    }
     
     @IBAction func rgbSlider(_ sender: UISlider) {
         colorPreview.backgroundColor = UIColor(
@@ -48,4 +43,15 @@ class ViewController: UIViewController {
         greenIndicator.text = String(format: "%.2f", greenSlider.value)
         blueIndicator.text = String(format: "%.2f", blueSlider.value)
     } // Вот здесь очень интересно получается) Каждый раз прикасаясь к любому из слайдеров, в соответствующий лэйбл передаётся значение. А два остальных слайдера значений не меняют, НО ОБНОВЛЯЮТ ИХ! Так как код воспроизводится каждый раз при касании слайдера! И это говорит мне о том что код не оптимальный, и его нужно оптимизировать
+    private func setColor() {
+        colorPreview.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        ) // Обязательно: Метод должен быть обязательно приватным! Располагаться он должен после аутлетов и IBAction'ов!
+    }
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
 }
